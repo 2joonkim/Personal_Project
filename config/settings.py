4603a0a1f4b6b61df331986e0de096ba2e2b537c
2joonkim <joon271694@gmail.com>
2025-01-31 18:23:55 +0900

    ✅ 프로젝트 완성
    완성 프로젝트 코드

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-os.environ['OPENAI_API_KEY'] = '입력'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
