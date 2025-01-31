c07351868d12bb31e6e744cb24916cc6d8644a58
2joonkim <joon271694@gmail.com>
2025-01-31 18:18:29 +0900

    ✅ 프로젝트 완료 코드 업로드
    완료 URL READ ME 파일 확인

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-os.environ['OPENAI_API_KEY'] = '입력git remote add origin https://github.com/2joonkim/Personal_Project.git'
+os.environ['OPENAI_API_KEY'] = '입력'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
